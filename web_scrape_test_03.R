9e83f82e351804fb77977a3a0eb2681f823b9734
Katie Murenbeeld <katiemurenbeeld@u.boisestate.edu>
2023-02-14 16:14:08 -0700

    updates and new web_scraping test code

--- web_scrape_test_03.R
+++ web_scrape_test_03.R
@@ -1,47 +1,27 @@
 library(tidyverse)
-library(rvest)
-library(jsonlite)
+#library(rvest)
+#library(jsonlite)
 library(stringr)
 library(xml2)
-library(RSelenium)
+#library(RSelenium)
 library(httr)
 
 url <- "https://infoweb.newsbank.com/apps/news/document-view?p=WORLDNEWS&t=continent%3ANorth%2BAmerica%21North%2BAmerica&sort=YMD_date%3AD&page=17&fld-base-0=alltext&maxresults=20&val-base-0=%22grizzly%20bears%22&docref=news/18CE672FEF7A3C60"
 
-#url %>%
-#  read_html()
-
-#main_body <- "document-view__body read document-view__body--ascii "
-
-#url_body <- url %>%
-#  read_html() 
-
-#url_body
-
-#xml_child(url_body, 2)
-
-#str_extract(url_body, regex("document-view__body read document-view__body--ascii ")) 
-
-
-#json_as_text <- html %>% html_element("body") %>% html_text()
-
-#document.querySelector("#document-view--ascii > div > div.document-view__content__inner > div.document-view__body.read.document-view__body--ascii")
-
-#test <- xml_text(xml_child(url_body, 2))
-#test
-url2 = 
 page <- GET(url)
+# You need to be connected to the VPN, otherwise you get text of the javascript telling you to log in. 
 page_text <- content(page, as = 'text')
 page_text
 lengths(regmatches(page_text, gregexpr("<p>", page_text)))
-grepl('document-view__body read document-view__body--ascii ', page_text, ignore.case=T)
-#str_extract(page_text, regex('(?<=<div class=\"document-view__body read document-view__body--ascii \">).'))
-#str_extract_all(page_text, regex("(?<=<p>).+(?=</p>)"))
-#str_replace_all(page_text, "<br/><br/>" , "")
-
-#str_remove(page_text, regex("(?<=<p>).+(?=</p>)")) 
+grepl('document-view__body read document-view__body--ascii ', page_text, ignore.case=T) # this is just a check.
+# when you look at the source code on the website, this is the division class that contains the actual article text
 text_body <- str_extract_all(page_text, regex("(?<=<p>).+(?=</p>)")) 
 text_body
+
+## From here I've been trying to clean up the text more, but having a bit of trouble
+## Since the urls all live within a column of the article_coding.csv, I imagine a script that loops through each url would be easy
+## to make, like you suggested before. 
+
 #text_body <- str_replace_all(text_body, "<br/><br/>" , " ")
 text_body <- str_replace_all(text_body, "[\r\n\t\f\v]" , " ")
 text_body
